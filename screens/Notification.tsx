ff9aba4903e7a4080d85542574ad2099082d4e27
Ngoc Phan <ngochbbs@gmail.com>
2023-02-22 07:23:28 +0700

    update package.json

--- screens/Notification.tsx
+++ screens/Notification.tsx
@@ -4,9 +4,12 @@ import { Text, View } from '../components/Themed';
 export default function Notification() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-     
+      <Text style={styles.title}>Tab Notification</Text>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
     </View>
   );
 }
